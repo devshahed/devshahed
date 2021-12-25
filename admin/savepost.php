b3d6344783364a588a00d113f7a4be7210024ef9
root <root@lazyCoder.shahed>
2021-12-26 01:47:52 +0600

    add.php

--- admin/savepost.php
+++ admin/savepost.php
@@ -2,14 +2,13 @@
     $title = $_POST['title'];
     $sdesc = $_POST['sdesc'];
     $desc = $_POST['desc'];
-
+    $filename = $_FILES['bannerImage']['name'];
+    $filename = explode('.', $filename);
     echo $title;
     echo $sdesc;
     echo $desc;
 
-    $folder = "./uploads/".date("Y-m-d H:i:s").".jpg";
-    echo $folder;
-    $filename = $_FILES['bannerImage']['name'];
+    $folder = "./uploads/".$filename[0].date("YmdHi").".jpg";
 	$tempname = $_FILES['bannerImage']["tmp_name"];
 
     if ( move_uploaded_file( $tempname, $folder ) ) {
